b25cd007a7a551d83af7c1bec341ad11e926d277
Sean McPherson <seanmcp25@gmail.com>
2017-07-28 11:47:51 -0400

    Updated JS to clear info and previous table on click; style and copy tweaks

--- js/main.js
+++ js/main.js
@@ -19,11 +19,6 @@ submitButton.addEventListener('click', function(e) {
   let placeVal = placeValue.value;
   let ogDenom = numerator;
 
-  ogState.innerHTML = "";
-  hideInfo.innerHTML = "";
-  hideInfo.style.margin = 0;
-  hideInfo.style.padding = 0;
-
   if (numerator <= 0) {
     alert("Please choose a number greater than zero");
   } else if (numerator > 1000){
@@ -31,6 +26,13 @@ submitButton.addEventListener('click', function(e) {
   } else if (placeVal > 20) {
     alert("Too many decimal places! Please choose a smaller number")
   } else {
+
+    //Hide instructions and previously generated table
+    ogState.innerHTML = "";
+    hideInfo.innerHTML = "";
+    hideInfo.style.margin = 0;
+    hideInfo.style.padding = 0;
+
     for (let i = numerator; i >= 0; i--) {
       let calc = (numerator / ogDenom) * 100;
       let roundedNum = calc.toFixed(placeVal);
